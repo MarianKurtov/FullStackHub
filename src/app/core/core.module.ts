3f67e450dd60ad010c0679b5f9820c87b80e1430
MarianKurtov <mariankurtov@gmail.com>
2023-07-26 10:12:07 +0300

    I deleted navigation component.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,24 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RxJSComponent } from './rx-js/rx-js.component';
-import { NavigationComponent } from './navigation/navigation.component';
 import { HomeComponent } from './home/home.component';
 import { RouterModule} from '@angular/router';
 import { PostModule } from '../post/post.module';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ThemeModule } from '../theme/theme.module';
+import { AboutComponent } from '../about/about.component';
 
 @NgModule({
   declarations: [
     RxJSComponent,
-    NavigationComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent
   ],
   exports: [
-    NavigationComponent,
     HomeComponent,
   ],
   imports: [
