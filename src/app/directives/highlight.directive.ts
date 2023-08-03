695cf7efc3d0d34a3f09ebedbef41eacf85f725d
MarianKurtov <mariankurtov@gmail.com>
2023-08-03 16:56:09 +0300

    Added highlight directive.

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,24 +1,28 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import {Directive, ElementRef, Input, OnChanges, Renderer2, SimpleChanges } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
-export class HighlightDirective {
+export class HighlightDirective implements OnChanges {
 
-  constructor(private el: ElementRef) {
-    //put default color
-    this.el.nativeElement.style.background = ''
+  @Input() color = 'green';
+
+  @Input() isHighLighted = false;
+
+  constructor(
+    private render: Renderer2,
+    private elementRef: ElementRef
+  ) { }
+
+  highLight(): void {
+    this.render.setStyle(
+      this.elementRef.nativeElement,
+      'backgroundColor',
+      this.isHighLighted ? this.color : 'inherit'
+    );
   }
 
-  @HostListener('mouseenter') onMouseEnter() {
-    this.highlight('yellow');
-  }
-
-  @HostListener('mouseleave') onMouseLeave() {
-    this.highlight('');
-  }
-
-  private highlight(color: string) {
-    this.el.nativeElement.style.backgroundColor = color;
+  ngOnChanges(changes: SimpleChanges): void {
+    this.highLight();
   }
 }
--- src/app/views/preparation/preparation.component.ts
+++ src/app/views/preparation/preparation.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { from, interval, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-preparation',
   templateUrl: './preparation.component.html',
   styleUrls: ['./preparation.component.css']
 })
-export class PreparationComponent {
+export class PreparationComponent implements OnInit{
 
+  clock: any;
+  public seeClock = false;
+  users = [ { name: 'Marian', age: 29 }, { name: 'Ivan', age: 31 } ];
+  isHighLighted = false;
+
+  highted(): void {
+    this.isHighLighted = !this.isHighLighted;
+  }
+
+  ngOnInit(): void {
+    // Interview
+    // myObservable = new Observable((observable) => {
+    //   setTimeout(() => {observable.next('1')}, 1000);
+    //   setTimeout(() => {observable.next('2')}, 2000);
+    //   setTimeout(() => {observable.next('3')}, 3000);
+    //   setTimeout(() => {observable.error(new Error("This user already exist!"))}, 4000);
+    //   setTimeout(() => {observable.next(Math.random()*100 )}, 5000);
+    //   });
+    // this.myObservable.subscribe((value) => {
+    //   console.log(value)
+    // }, (error) => {
+    //   alert(error.message)
+    // });
+
+    //fromEvent -> console log
+    // const nameInput = document.getElementById("name-input");
+    // // @ts-ignore
+    //  const clickStream = fromEvent(nameInput, 'keyup');
+    //
+    //  clickStream.pipe(
+    //    map(e => e.target)
+    //  ).subscribe(console.log)
+
+    // Clock
+    const clock = document.getElementById("clock");
+
+    interval(1000).pipe(
+      map(() => new Date)
+      ).subscribe(
+      date => {
+        const h = date.getHours();
+        const m = date.getMinutes();
+        const s = date.getSeconds();
+        this.clock = `${h}:${m}:${s}`;
+      }
+    )
+    // End Clock
+
+    const users$ = from(this.users).subscribe(console.log);
+
+
+  }
 }
